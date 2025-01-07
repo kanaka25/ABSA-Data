94f31bf375b183fcd6b1c6ef8a75c80c3266b05e
hopiemnisi@gmail.com <hopiemnisi@gmail.com>
2025-01-07 10:11:35 +0200

    rearanging of questions

--- Customer Data.session.sql
+++ Customer Data.session.sql
@@ -6,7 +6,39 @@
 -- GROUP BY c.code, i.descript
 -- ORDER BY c.code, i.descript
 
--- Question 2: What is the relationship between sex_code and number of transactions
+-- Question 2: How does the number of transactions vary across income groups?
+
+-- SELECT i.descript, count(t.cust_id)
+-- FROM customers c
+-- JOIN income_group i
+-- ON i.code = c.code
+-- JOIN transaction t
+-- ON c.cust_id = t.cust_id
+-- GROUP BY i.code, i.descript
+
+--Question 3: Which product code (account type) has the most significant cash flow?
+
+-- SELECT
+--     EXTRACT(YEAR FROM record_date) AS transaction_year,
+--     EXTRACT(MONTH FROM record_date) AS transaction_month,
+--     prod_code  AS account_type, 
+--     SUM(amt) as cash_flow
+-- FROM transaction
+-- WHERE prod_code = 'CHEQ'
+-- GROUP BY prod_code, transaction_year, transaction_month
+-- ORDER BY transaction_year, transaction_month;
+
+-- SELECT
+--     EXTRACT(YEAR FROM record_date) AS transaction_year,
+--     EXTRACT(MONTH FROM record_date) AS transaction_month,
+--     prod_code  AS account_type, 
+--     SUM(amt) as cash_flow
+-- FROM transaction
+-- WHERE prod_code = 'SAVE'
+-- GROUP BY prod_code, transaction_year, transaction_month
+-- ORDER BY transaction_year, transaction_month;
+
+-- Question 4: What is the relationship between sex_code and number of transactions
 
 -- SELECT
 -- CASE 
@@ -31,38 +63,6 @@
 -- JOIN transaction t
 -- ON c.cust_id = t.cust_id
 -- GROUP BY c.sex_code
-
--- Question 3: How does the number of transactions vary across income groups?
-
--- SELECT i.descript, count(t.cust_id)
--- FROM customers c
--- JOIN income_group i
--- ON i.code = c.code
--- JOIN transaction t
--- ON c.cust_id = t.cust_id
--- GROUP BY i.code, i.descript
-
---Question 4: Which product code (account type) has the most significant cash flow?
-
--- SELECT
---     EXTRACT(YEAR FROM record_date) AS transaction_year,
---     EXTRACT(MONTH FROM record_date) AS transaction_month,
---     prod_code  AS account_type, 
---     SUM(amt) as cash_flow
--- FROM transaction
--- WHERE prod_code = 'CHEQ'
--- GROUP BY prod_code, transaction_year, transaction_month
--- ORDER BY transaction_year, transaction_month;
-
--- SELECT
---     EXTRACT(YEAR FROM record_date) AS transaction_year,
---     EXTRACT(MONTH FROM record_date) AS transaction_month,
---     prod_code  AS account_type, 
---     SUM(amt) as cash_flow
--- FROM transaction
--- WHERE prod_code = 'SAVE'
--- GROUP BY prod_code, transaction_year, transaction_month
--- ORDER BY transaction_year, transaction_month;
 
 --Question 5: How does the average account balanace vary across different income groups
 
